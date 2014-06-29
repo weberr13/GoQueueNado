151be27592cc66e9793ebedbf134f34e2f27b4dd
Robert Weber <weberr@webserver.webermom.com>
2014-06-29 17:13:08 -0600

    Look at this, we used a push pull queue!!!

--- helloworld.go
+++ helloworld.go
@@ -2,10 +2,31 @@ package main
 
 import (
    "fmt"
-   zmq "../zmq4"
+   zmq "github.com/pebbe/zmq4"
 )
 
 func main() {
-   fmt.Println("Hello World")
+	client, err := zmq.NewSocket(zmq.PULL)
+	if err != nil {
+		panic(err)
+	}
+	server, err := zmq.NewSocket(zmq.PUSH)
+	if err != nil {
+		panic(err)
+	}
+
+	server.Bind("ipc:///tmp/helloworld.ipc")
+        client.Connect("ipc:///tmp/helloworld.ipc")
+
+	_, err = server.Send("hello",0)
+        if err != nil  {
+		panic(err)
+	}
+        received, err := client.Recv(0)
+        if err != nil  {
+		panic(err)
+	}
+	fmt.Println(received + " World")
+ 
 }
    
